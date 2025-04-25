b7de15f733a535bb5cf5720697fbf067ccadb50c
harrychen1995 <harrychen1995@yahoo.com>
2025-04-25 13:04:30 -0500

    add style to all modal header

--- src/Home/demoVideoModel.tsx
+++ src/Home/demoVideoModel.tsx
@@ -8,22 +8,19 @@ interface VideoModalProps {
 const VideoModal: React.FC<VideoModalProps> = ({ isOpen, onClose }) => {
   return (
     <Modal show={isOpen} onHide={onClose} centered size="lg">
-      <Modal.Header
-        closeButton
-    className="video-modal-header"
-      >
+      <Modal.Header closeButton>
         <Modal.Title>
-          <h4 className="fw-bold brand">Demo Video</h4>
+          <h4 className="fw-bold brand-color">Demo Video</h4>
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body style={{ padding: 0, height: "50vh"}}>
-        <div style={{ width: "100%", height: "100%"}}>
+      <Modal.Body style={{ padding: 0, height: "50vh" }}>
+        <div style={{ width: "100%", height: "100%" }}>
           <iframe
             src="https://www.youtube.com/embed/f9qvVsxjZVw"
             title="Demo Video"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
-            style={{ width: "100%", height: "100%", border:"none" }}
+            style={{ width: "100%", height: "100%", border: "none" }}
           />
         </div>
       </Modal.Body>
